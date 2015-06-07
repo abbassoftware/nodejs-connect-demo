d91ef1e8fc55837ab2e0dada273073c00bc0a27d
Abbas <abbas.s786@yahoo.co.in>
2015-06-07 16:39:44 +0530

    Added the logging middleware

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var connect = require("connect");
+var url = require('url');
 var app = connect();
 
 function sayHello(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
+    res.write('Write first chunk. ');
+    res.write('Write second chunk. ');
     res.end('Hello Connect');
 }
 
+function loggingMiddleware(req, res, next) {
+    console.log("The request method is : " + req.method );  
+    console.log("The request url is : " + req.url );  
+    var queryData = url.parse(req.url, true).query;
+    console.log("The query parameters are : " + queryData.name );  
+    next();
+}
+
 app
+.use(loggingMiddleware)
 .use(sayHello)
 .listen(3031);
 
