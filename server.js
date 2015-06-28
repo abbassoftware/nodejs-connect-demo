5cb7af0edf9009ffc95dfebe43c5e3506f1a8bc7
Abbas Suterwala <abbas.s786@yahoo.co.in>
2015-06-28 14:03:20 +0530

    Added authentication for admin section

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var connect = require("connect");
 var url = require('url');
 var app = connect();
+var authJsonObj = require("./authDetails.json");
 
 function sayHello(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
@@ -10,15 +11,50 @@ function sayHello(req, res, next) {
 }
 
 function loggingMiddleware(req, res, next) {
-    console.log("The request method is : " + req.method );  
-    console.log("The request url is : " + req.url );  
+    console.log("The request method is : " + req.method );
+    console.log("The request url is : " + req.url );
     var queryData = url.parse(req.url, true).query;
-    console.log("The query parameters are : " + queryData.name );  
+    console.log("The query parameters are : " + queryData.name );
     next();
 }
 
+function authenticateAdmin(req, res, next) {
+    console.log("authenticateAdmin");
+    var authorization = req.headers.authorization;
+    //if the Authorization header is not present return error.
+    if (!authorization) return returnAuthError(res);
+
+    var parts = authorization.split(' ');
+
+    //Check the Authorization header contains both the parts.
+    if (parts.length !== 2) return returnAuthError(res);
+
+    //Check the Authorization header Scheme is correct.
+    var scheme = parts[0];
+    if ('Basic' != scheme) return returnAuthError(res);
+
+    //Credentials will be base64 encoded. After decoding they will be in the format username:password
+    var credentials = new Buffer(parts[1], 'base64').toString()
+    var index = credentials.indexOf(':');
+
+    var user = credentials.slice(0, index)
+    var pass = credentials.slice(index + 1);
+
+    //If the password does not match return error.
+    if(authJsonObj[user] != pass) return returnAuthError(res);
+
+    //Auth is complete pass to the next handler.
+    next();
+}
+
+function returnAuthError(res) {
+  res.statusCode = 401;
+  res.end('Unauthorized     ');
+};
+
 app
 .use(loggingMiddleware)
+.use('/admin', authenticateAdmin)
 .use(sayHello)
 .listen(3031);
 
